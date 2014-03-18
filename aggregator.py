7f4f5e759a34357f6677bb0841167d6792399ecb
Alexander Loutsenko <alex.chorry@gmail.com>
2014-03-18 19:15:43 +0400

    aggregator

---
+++ aggregator.py
@@ -0,0 +1,48 @@
+class Aggregator:
+    #max amount of results in buffer
+    maxBufferSize = 100
+    bufferCount = 0
+    buffer = []
+    times = 0
+    #seconds
+    timeInterval = 1
+
+    def aggregate(self, logTaskResultDict):
+        for logTaskResult in logTaskResultDict:
+            if len(self.buffer) >= self.maxBufferSize:
+                self.flushBuffer()
+
+            self.bufferCount += 1
+            self.buffer.append(logTaskResult)
+
+    def flushBuffer(self):
+        self.times += 1
+        upstreamAvg = 0
+        cachedQueries = 0
+        upstreamQueries = 0
+
+        for bufferEl in self.buffer:
+
+            if bufferEl.type == 'upstreamAvg':
+                upstreamAvg += bufferEl.result if bufferEl.result is not None else 0
+                pass
+            elif bufferEl.type == 'cachedQuery':
+                if bufferEl.result == 1:
+                    cachedQueries += 1
+                else:
+                    upstreamQueries += 1
+                pass
+            else:
+                raise Exception("Unknown log result?")
+
+        print "upstreamAvg=%f, cachedQ=%d, upstreamQ=%d" % (upstreamAvg, cachedQueries, upstreamQueries)
+        print "---buffer has flushed %d times!---\n" % self.times
+
+        self.bufferCount = 0
+        self.buffer = []
+
+    def aggregateCachedQueriesByTimeInterval(self):
+        pass
+
+    def aggregateUpstreamAvgByTimeInterval(self):
+        pass
--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
-
+from collections import Counter
+import pickle
+from logTask import logTask, logTaskResult
+from aggregator import Aggregator
 import re
 import time
+import sys
 from pprint import pprint
+import ctypes
+
+import datetime, bisect, collections
 
 """ These symbols in nginx_log_format may break regex, if are left unattended"""
 LOG_SPECIAL_SYMBOLS = {
@@ -19,25 +26,25 @@ LOG_PARAMS = {
     '\$connection': 'NOT_IMPLEMENTED',  #порядковый номер соединения
     '\$connection_requests': '(?P<connection_requests>\d+)',  #текущее число запросов в соединении (1.1.18)
     '\$host': '(?P<host>[A-Za-z0-9-\.]+)',
-    '\$http_referer': '(?P<http_referer>[\d\D]+)',
-    '\$http_user_agent': '(?P<http_user_agent>[\d\D]+)',
+    '\$http_referer': '(?P<http_referer>[\d\D]+|)',  #referer cAN by empty
+    '\$http_user_agent': '(?P<http_user_agent>[\d\D]+|)',  #agent can be empty
     #'\$http_x_forwarded_for': '(?P<http_x_forwarded_for>[\d\D.,\-\s]+)',
     '\$http_x_forwarded_for': '(?P<http_x_forwarded_for>[\d\D]+|)',
     '\$msec': '(?P<msec>\d+)',  #время в секундах с точностью до миллисекунд на момент записи в лог
     '\$pipe': '(?P<pipe>[.p])',  #“p” если запрос был pipelined, иначе “.”
     '\$remote_addr': '(?P<remote_addr>\d+.\d+.\d+.\d+)',
-    '\$remote_user': '(?P<remote_user>\W)',
-    '\$request': '(?P<request>[A-Z]{3,4} [\d\D]+ HTTP/[0-9.]+)',
+    '\$remote_user': '(?P<remote_user>[\D\d]+)',
+    '\$request': '(?P<request_request_method>[A-Z]+) (?P<request_request_uri>[\d\D]+) (?P<request_request_http_version>HTTP/[0-9.]+)',
     '\$request_length': '(?P<request_length>\d+)',  #длина запроса (включая строку запроса, заголовок и тело запроса)
     '\$request_time': '(?P<request_time>[\d.]+)',
     #время обработки запроса в секундах с точностью до миллисекунд; время, прошедшее с момента чтения первых байт от клиента до момента записи в лог после отправки последних байт клиенту
     '\$status': '(?P<status>\d+)',  #статус ответа
     '\$time_iso8601': 'NOT_IMPLEMENTED',  #локальное время в формате по стандарту ISO 8601
-    '\$time_local': '(?P<time_local>[0-3][0-9]/[A-Za-z]{3}/[0-9]{4}:[0-2]{2}:[0-5][0-9]:[0-5][0-9] [-+0-9]+)',
+    '\$time_local': '(?P<time_local>[0-3][0-9]/[A-Za-z]{3}/[0-9]{4}:[0-9]{2}:[0-5][0-9]:[0-5][0-9] [-+0-9]+)',
     #локальное время в Common Log Format
     '\$upstream_addr': '(?P<upstream_addr>[\-A-Za-z0-9.:, ]+)',  #ip:port and unix:socket-path
-    '\$upstream_response_time': '(?P<upstream_response_time>[-.\d: ]+)',
-    '\$upstream_status': '(?P<upstream_status>[-0-9: ]+)',
+    '\$upstream_response_time': '(?P<upstream_response_time>[-.\d,: ]+)',
+    '\$upstream_status': '(?P<upstream_status>[\-0-9,: ]+)',
 }
 
 
@@ -68,21 +75,116 @@ def parseLogLine(compiledReObject, text, displayResult=True):
     result = compiledReObject.finditer(text)
     group_name_by_index = dict([(v, k) for k, v in compiledReObject.groupindex.items()])
 
+    resultObj = {}
     iteratorSize = 0
     for match in result:
         iteratorSize += 1
         for group_index, group in enumerate(match.groups()):
-            if group and displayResult:
-                #print group_index, group
-                print "%s : %s" % (group_name_by_index[group_index + 1], group)
-                #time.sleep(1)
+            if group:
+                if displayResult:
+                    print "%s : %s" % (group_name_by_index[group_index + 1], group)
+                resultObj[group_name_by_index[group_index + 1]] = group
+
     if iteratorSize == 0:
-        print "Could not parse [ %s ]" % text
+        #raise Exception("Could not parse [ %s ]" % text)
+        print ("Could not parse [ %s ]" % text)
+        return None
 
-    return iteratorSize
+    #TODO: do something with this date
+    resultObj['t_date_local'] = resultObj['time_local'][:11]
+    resultObj['t_time_local'] = resultObj['time_local'][12:20]
+    if resultObj['upstream_response_time'] != '-':
+        if resultObj['upstream_response_time'].find(':') != -1:
+            splitKey = ':'
+        else:
+            splitKey = ','
+        resultObj['upstreams'] = resultObj['upstream_response_time'].split(splitKey)
 
+    return resultObj
+
+def tryUpstreamsRespAvg(v, default=0):
+    try:
+        ret = float(v)
+    except ValueError:
+        ret = float(0)
+
+    return ret
+
+
+""" Processes all attached tasks to parsed log object, if applicable
+"""
+def processLogTasks(object):
+    result = []
+    for task in getAttachedTaskLists():
+        taskResult = task.process(object)
+
+        if taskResult is not None:
+            result.append(taskResult)
+
+    if len(result) > 0:
+        return result
+    else:
+        return None
+
+
+def getAttachedTaskLists():
+    try:
+        return attachedTasks
+    except NameError:
+        return ()
+
+
+attachedTasks = []
+
+'''
+----------- TASKS -----------
+'''
+def isCachedQuery(self, logObject):
+    cacheKey = 'missed' if (logObject['upstream_addr'] != '-') else 'hit'
+
+    r = logTaskResult()
+    r.setType('cachedQuery')
+    r.setResult(
+        0 if (logObject['upstream_addr'] != '-') else 1
+    )
+    r.setTime(logObject['t_time_local'][:8])
+
+    return r
+
+def getUpstreamAvgResp(self, logObject):
+    upstreamRespAvg = 0
+    r = logTaskResult()
+
+    if 'upstreams' in logObject:
+        upstreamRespAvg = \
+            reduce(
+                lambda res, v:
+                tryUpstreamsRespAvg(v) + res,
+                logObject['upstreams'], 0
+            ) / len(logObject['upstreams'])
+
+    r.setType('upstreamAvg')
+    r.setResult(upstreamRespAvg)
+    r.setTime(logObject['t_time_local'][:8])
+    return r
+
+
+cqLogTask = logTask()
+cqLogTask.attachTask(isCachedQuery)
+attachedTasks.append(cqLogTask)
+
+upAvgLogTask = logTask()
+upAvgLogTask.attachTask(getUpstreamAvgResp)
+attachedTasks.append(upAvgLogTask)
+
+aggregator = Aggregator()
+
+swapFileName = 'worker.swap'
 
 if __name__ == '__main__':
+
+    print "Start @ " + time.asctime()
+    scriptTimeStart = time.time()
     nginxLogFormat = ('$remote_addr $host $remote_user [$time_local] $request '
                       '"$status" $body_bytes_sent "$http_referer" '
                       '"$http_user_agent" "$http_x_forwarded_for" '
@@ -92,15 +194,33 @@ if __name__ == '__main__':
 
     object = re.compile(rePattern)
 
-    logFileName = 'custom_short.log'
+    logFileName = 'bigLog.log'
 
     linenum = 0
 
-    #parseLogLine(compiledReObject=object, text=myString)
+    hitsPerTimeInterval = Counter()
+    cachedQuery = Counter()
+    upstreamResp = Counter()
+    upstreamList = []
 
+    a = 1
     with open(logFileName) as f:
-        for line in f:
-            linenum += 1
-            parseLogLine(compiledReObject=object, text=line, displayResult=False)
 
-    print "the end"
+        for line in f:
+            if line != '':
+                linenum += 1
+                processedObj = parseLogLine(compiledReObject=object, text=line, displayResult=False)
+
+                r = None
+                if processedObj is not None:
+                    r = processLogTasks(processedObj)
+                if r is not None:
+                    aggregator.aggregate(r)
+
+aggregator.flushBuffer()
+print "End @ " + time.asctime()
+
+scriptTimeEnd = time.time()
+scriptTimeTotal = scriptTimeEnd - scriptTimeStart
+print "Running time: %f s" % scriptTimeTotal
+
